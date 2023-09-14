396def26e9025fdf65c5f419f03678610f53c7f2
froglegseternal <quinng@umich.edu>
2023-09-14 17:35:24 -0400

    Class period on 9/14.

--- lab-01_universiality.py
+++ lab-01_universiality.py
@@ -15,7 +15,7 @@ nx, ny = 3, 5 # Number of cells in X and Y direction.
 prob_spread = 1.0 # Chance to spread to adjacent cells.
 prob_bare = 0.0 # Chance of cell to start as a bare patch.
 prob_start = 0.0 # Chance of cell to start on fire.
-iterations = 2 # Number of times to iterate time.
+iterations = 5 # Number of times to iterate time.
  
 
 def is_burning(i, j, forest_array):
@@ -85,8 +85,8 @@ history_matrix = np.zeros([iterations+1, nx, ny], dtype =int) # 3-D array storin
 # type in our array to integers only.
 forest = np.zeros([nx, ny], dtype =int) + 2
 
-# Set the center cell to "burning":
-forest[int(nx/2), int(ny/2)] = 3
+# Set a random cell to "burning":
+forest[int(np.random.rand() * nx), int(np.random.rand() * ny)] = 3
 
 # Create an array of randomly generated number of range [0, 1):
 isbare = np.random.rand(nx, ny)
@@ -118,6 +118,7 @@ def do_iterate(cur_iter, iter_num):
     history_matrix[cur_iter] = forest
     cur_iter = cur_iter + 1 # Locally defined iteration count.
 
+
     # Loop in the "x" direction:
     for i in range(nx):
         # Loop in the "y" direction:
@@ -127,13 +128,13 @@ def do_iterate(cur_iter, iter_num):
                 forest[i, j] = 1
                 isbare[i, j] = True
                 edge_status = cell_is_edge(i, j) # Now that we know the cell is burning, figure out if our current cell is an edge cell, and if so, in what way.
-                if 'left' not in edge_status and forest[i,j-1] != 1:
+                if 'left' not in edge_status and forest[i,j-1] != 1 and np.random.rand() < prob_spread:
                     forest[i,j-1] = 3
-                if 'right' not in edge_status and forest[i,j+1] != 1:
+                if 'right' not in edge_status and forest[i,j+1] != 1 and np.random.rand() < prob_spread:
                     forest[i,j+1] = 3
-                if 'bottom' not in edge_status and forest[i+1, j] != 1:
+                if 'bottom' not in edge_status and forest[i+1, j] != 1 and np.random.rand() < prob_spread:
                     forest[i+1,j] = 3
-                if 'top' not in edge_status and forest[i-1, j] != 1:
+                if 'top' not in edge_status and forest[i-1, j] != 1 and np.random.rand() < prob_spread:
                     forest[i-1,j] = 3
                 
 
@@ -142,36 +143,72 @@ def do_iterate(cur_iter, iter_num):
                 continue
     do_iterate(cur_iter, iter_num) # Recursion call
             
+def do_process(num_states, matrix):
+    '''
+    After we have run our model, this is the code that will actually process the model. It takes in the number of states in the given array, and the array to process.
+    The way it works is by creating several arrays, each of which represent the change in a certain value over time. These values, in turn, are the different states
+    the cells can have. It returns an array of these arrays. It assumes that the states are integers - given how the rest of the code is laid out, this should not
+    be an issue, but is not the ideal representation.
 
-do_iterate(0, iterations)
+    Parameters
+    ==========
+    num_states: int
+        The number of different states in the array
+    matrix: numpy array
+        A 3-dimensional numpy array that is to be processed.
+    Returns
+    ======
+    state_arrays: numpy array
+        A 2-dimensional array, with each row representing a different state, and each column representing a point in time.
+    '''
+    state_arrays = np.zeros([num_states, iterations]) # Define our array that we will eventually return.
+    for x in range(iterations): # Iterate over each moment in time.
+        for i in range(nx): # Iterate over each row in the matrix we're counting.
+            for j in range(ny): # Iterate over each column in the matrix we're counting.
+                state_arrays[matrix[x][i][j] - 1][x] = state_arrays[matrix[x][i][j] - 1][x] + 1 # Increment the count for the state this cell is in at this specific moment in time.
+    return state_arrays
 
-# Generate a custom segmented color map for this project.
-# Can specify colors by names and then create a colormap that only uses
-# those names. There are 3 fundamental states - barren, burning, and not yet on fire.
-# Therefore, three colors are needed. Color info can be found at the following:
-# https://matplotlib.org/stable/gallery/color/named_colors.html
-forest_cmap = ListedColormap(['tan', 'darkgreen', 'crimson'])
-# Create figure and set of axes:
-fig, ax = plt.subplots(1,1)
+def plot_last_moment():
+    '''
+    The function responsible for checking the state of the forest array at the last moment in time. This is its own function to allow for scalability.
+    '''
+    # Generate a custom segmented color map for this project.
+    # Can specify colors by names and then create a colormap that only uses
+    # those names. There are 3 fundamental states - barren, burning, and not yet on fire.
+    # Therefore, three colors are needed. Color info can be found at the following:
+    # https://matplotlib.org/stable/gallery/color/named_colors.html
+    forest_cmap = ListedColormap(['tan', 'darkgreen', 'crimson'])
+    # Create figure and set of axes:
+    fig, ax = plt.subplots(1,1)
 
-# Set graph title
-ax.set_title("Forest Having Burnt for " + str(iterations) + " Iterations.")
+    # Set graph title
+    ax.set_title("Forest Having Burnt for " + str(iterations) + " Iterations.")
 
-# Set axes titles.
-ax.set_xlabel("X Direction (m)")
-ax.set_ylabel("Y Direction (m)")
+    # Set axes titles.
+    ax.set_xlabel("X Direction (m)")
+    ax.set_ylabel("Y Direction (m)")
 
-# Because of the way that numpy arrays are indexed, we need to explicitly set the axes.
-plt.xlim(0,ny)
-plt.ylim(0,nx)
+    # Because of the way that numpy arrays are indexed, we need to explicitly set the axes.
+    plt.xlim(0,ny)
+    plt.ylim(0,nx)
 
-# Given the "forest" object, a 2D array that contains numbers 1, 2, or 3,
-# Plot this using the "pcolor" method. Need to use our color map as well as set
-# both *vmin* and *vmax*.
-ax.pcolor(forest, cmap=forest_cmap, vmin=1, vmax=3)
-ax.imshow(forest)
+    # Given the "forest" object, a 2D array that contains numbers 1, 2, or 3,
+    # Plot this using the "pcolor" method. Need to use our color map as well as set
+    # both *vmin* and *vmax*.
+    ax.pcolor(forest, cmap=forest_cmap, vmin=1, vmax=3)
+    ax.imshow(forest)
 
-#norm = colors.Normalize(vmin=1, vmax=3)
-#fig.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=forest_cmap), cax=ax[0], orientation='horizontal',shrink=9)
+    plt.show()
 
-plt.show()
+def plot_statistics(stat_matrix):
+    '''
+    The function responsible for plotting our statistics once we have a functional model. Takes in the matrix of calculated statistics.
+
+    Parameters
+    ==========
+    stat_matrix: numpy array
+        A 2-dimensional array that is the output of the do_process function and represents count over time.
+    '''
+
+do_iterate(0, iterations) # Run the model
+statistics = do_process(3, history_matrix) # Do the statistics
