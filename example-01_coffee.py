d12271a2ab2e6557943787450dfd590d6411ca23
froglegseternal <quinngdavid@gmail.com>
2023-09-26 16:41:50 -0400

    started lab 2
    Needs work

--- example-01_coffee.py
+++ example-01_coffee.py
@@ -41,26 +41,65 @@ def time_to_temp(T_targ, k=1/300., T_env=20, T_init=90):
     '''
     return (-1/k) * np.log((T_targ-T_env)/(T_init-T_env))
 
-#Solve our coffee question!
-T_cream = solve_temp(time, T_init=85)
-T_nocrm = solve_temp(time, T_init=90)
+def solve_coffee_simple():
+    '''
+    Generate the analytical solution for our coffee problem.
+    '''
+    #Solve our coffee question!
+    T_cream = solve_temp(time, T_init=85)
+    T_nocrm = solve_temp(time, T_init=90)
 
-#Get time to drinkable temp
-t_cream = time_to_temp(60, T_init=85) #Add cream right away.
-t_nocrm = time_to_temp(60, T_init=90) #Add cream once at 60 deg.
-t_smart = time_to_temp(65, T_init=90) #Put cream in at 65 deg.
+    #Get time to drinkable temp
+    t_cream = time_to_temp(60, T_init=85) #Add cream right away.
+    t_nocrm = time_to_temp(60, T_init=90) #Add cream once at 60 deg.
+    t_smart = time_to_temp(65, T_init=90) #Put cream in at 65 deg.
 
-#Create figure and axes objects:
-fig, ax = plt.subplots(1,1)
+    #Create figure and axes objects:
+    fig, ax = plt.subplots(1,1)
 
-#Plot lines and label
-ax.plot(time, T_nocrm, label='No cream till cool.')
-ax.plot(time, T_cream, label="Cream right away")
+    #Plot lines and label
+    ax.plot(time, T_nocrm, label='No cream till cool.')
+    ax.plot(time, T_cream, label="Cream right away")
 
-ax.axvline(t_nocrm, c='red',ls='--', label='No Cream: T=60')
-ax.axvline(t_cream, c='blue',ls='--', label='Cream: T=60')
-ax.axvline(t_smart, c='gray',ls='--', label='No Cream: T=65')
+    ax.axvline(t_nocrm, c='red',ls='--', label='No Cream: T=60')
+    ax.axvline(t_cream, c='blue',ls='--', label='Cream: T=60')
+    ax.axvline(t_smart, c='gray',ls='--', label='No Cream: T=65')
 
-ax.legend(loc='best')
+    ax.legend(loc='best')
 
-fig.tight_layout()
+    fig.tight_layout()
+
+def hot(T, k, Tenv):
+    '''
+    Given current temperature (T), cooling rate (k) and the environmental
+    temperature (Tenv), return the derivative of Temperature of the cooling
+    body.
+    '''
+
+    return -k*(T-Tenv)
+
+def euler(time, T_init, deltaT=60, tstart=0, tstop=600, k=1/300,
+     Tenv=21, Tstop=60):
+    '''
+    Solve the cooling equation given a time array, 'time', with times
+    evenly spaced by some delta-T and in units of seconds.
+
+    Also require T_init, the initial temperature of the body in Celsius.
+    '''
+    # Create time array:
+    time = np.arange(0, tstart, tstop)
+
+    # Create solution array:
+    T_sol = np.zeros(time.size)
+    T_sol[0] = T_init # Set initial temp.
+
+    # Iterate through our solver:
+    for i in range(time.size - 1):
+        T_sol[i+1] = T_sol[i] + deltaT * hot(T_sol[i], k=k, Tenv=Tenv) 
+
+        if T_sol[i+1] <= Tstop:
+            T_sol = T_sol[:i+2]
+            time = time[:i+2]
+            break
+        
+    return time, T_sol
---
+++ lab2.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+# Standard imports as well as scipy:
+from scipy.integrate import solve_ivp
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Ndt_comp(t, N, a=1, b=2, c=1, d=3):
+    '''
+    This function calculates the Lotka-Volterra competition equations for
+    two species. Given normalized populations, `N1` and `N2`, as well as the
+    four coefficients representing population growth and decline,
+    calculate the time derivatives dN_1/dt and dN_2/dt and return to the
+    caller.
+
+    This function accepts `t`, or time, as an input parameter to be
+    compliant with Scipy's ODE solver. However, it is not used in this function.
+
+    Parameters
+    ----------
+    t: float
+        The current time (not used here).
+    N: two-element list
+        The current value of N1 and N2 as a list (e.g., [N1, N2]).
+    a, b, c, d: float, defaults=1, 2, 1, 3
+        The value of the Lotka-Volterra coefficients.
+
+    Returns
+    -------
+    dN1dt, dN2dt: floats
+        The time derivatives of `N1` and `N2`.
+    '''
+
+    # Here, N is a two element list such that N1=N[0] and N2=N[1]
+    dN1dt = a*N[0]*(1-N[0]) - b*N[0]*N[1]
+    dN2dt = c*N[1]*(1-N[1]) - d*N[1]*N[0]
+    
+    return dN1dt, dN2dt
+
+def euler_solve(func, N1_init=.5, N2_init=.5, dT=.1, t_final=100.0):
+    '''
+    Solve the Lotka-Volterra competition and predator/prey equations using
+    Euler's Method. Given a python function, the initial values of N1 and N2,
+    the time step, and the final time, return the time elapsed as an array and
+    the normalized population density solutions.
+
+    Parameters
+    ----------
+    func: function
+        A python function that takes `time`, [`N1`, `N2`] as inputs and
+        returns the time derivative of N1 and N2.
+    N1_init, N2_init: float
+        Initial conditions for `N1` and `N2`, ranging from (0, 1]
+    dT: float, default=0.1
+        Timestep in years.
+    t_final: float, default=100
+        Integrate until this value is reached, in years.
+
+    Returns
+    -------
+    time: Numpy array
+        Time elapsed in years.
+    N1, N2: Numpy arrays
+        Normalized population density solutions
+    '''
+
+    # Create time array.
+    time = np.arange(0, t_final, dT)
+    
+    # Create containers for the solution, set initial condition.
+    N1 = np.zeros(time.size)
+    N2 = np.zeros(time.size)
+    N1[0] = N1_init
+    N2[0] = N2_init
+
+    # Integrate forward:
+    for i in range(1, time.size):
+        dN1, dN2 = func(i, [N1[i-1], N2[i-1]])
+        N1[i] = N1[i-1] + dT*dN1
+        N2[i] = N2[i-1] + dT*dN2
+
+    return time, N1, N2
+
+def solve_rk8(func, N1_init=.5, N2_init=.5, dT=10, t_final=100.0, a=1, b=2, c=1, d=3):
+    '''
+    Solve the Lotka-Volterra competition and predator/prey equations using Scipy's ODE
+    class and the adaptive step 8th order solver.
+
+    Parameters
+    ----------
+    func: function
+        A python function that takes `time`, [`N1`, `N2`] as inputs and returns the time
+        derivative of N1 and N2.
+    N1_init, N2_init: float
+        Initial conditions for `N1` and `N2`, ranging from (0,1]
+    dT: float, default=10
+        Largest timestep allowed in years.
+    t_final: float, default=100
+        Integrate until this value is reached, in years.
+    a, b, c, d: float, default=1, 2, 1, 3
+        Lotka-Volterra coefficient values
+    
+    Returns
+    -------
+    time: Numpy array
+        Time elapsed in years
+    N1, N2: Numpy arrays
+        Normalized population density solutions.
+    '''
+
+    # Configure the initial value problem solver
+    result = solve_ivp(func, [0, t_final], [N1_init, N2_init], args=[a, b, c, d],
+                       method='DOP853', max_step=dT)
+    
+    # Perform the integration
+    time, N1, N2 = result.t, result.y[0, :], result.y[1, :]
+
+    # Return values to caller.
+    return time, N1, N2
+
+def problem_one():
+    '''
+    This fuction solves the first problem in the assignment. 
+    '''
+
+    # Define our constants for this problem.
+    a = 1
+    b = 2
+    c = 1
+    d = 3
+    max_T = 100
+    init_N1 = 0.3
+    init_N2 = 0.6
+
+    # Define our changing values.
+    comp_step = 1 # Euler time step for the competition model
+    prey_step = 0.05 # Euler time step for the prey model
+
+    # Run our models for the competition model.
+    time_euler, N1_euler, N2_euler = euler_solve(Ndt_comp, init_N1, init_N2, comp_step, max_T)
+    time_rk, N1_rk, N2_rk = euler_solve(Ndt_comp, N1_init=init_N1, N2_init=init_N2, t_final=max_T)
+
+    fig, axes = plt.subplots(1, 1, figsize=(10, 6))
+    axes.plot(time_euler, N1_euler)
+    axes.plot(time_euler, N2_euler)
+    axes.plot(time_rk, N1_rk)
+    axes.plot(time_rk, N2_rk)
+
+    fig.show()
+
+problem_one()
